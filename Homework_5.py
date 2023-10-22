25d8c2ce1c49bc63e5b82ae6b20167aa9a881b7b
travychka <sheremeta1anastasiya@gmail.com>
2023-10-22 22:39:43 +0200

    Lesson 10.10. Homework

---
+++ Homework_5.py
@@ -0,0 +1,37 @@
+#1
+#my_str = "What is your name?"
+
+#a
+#print(my_str[2])
+
+#b
+#print(my_str[-2])
+
+#c
+#print(my_str[ : 5])
+
+#d
+#print(my_str[ : -2])
+
+#e
+#print(my_str[ : : 2])
+
+#f
+#print(my_str[1: : 2])
+
+#g
+#print(my_str[ : :-1])
+
+#h
+#print(my_str[ : :-2])
+
+#i
+#print(len(my_str))
+
+#2
+#my_str_2 = "You set your own mood"
+#space = my_str_2.count(" ")
+#words = space + 1
+#print(words)
+
+#3
