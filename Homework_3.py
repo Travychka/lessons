d7c1349955cc18aa238ebb0c0ded3dfbcc00412b
travychka <sheremeta1anastasiya@gmail.com>
2023-11-04 21:25:45 +0100

    Lesson 10.10. Homework

---
+++ Homework_3.py
@@ -0,0 +1,26 @@
+#1
+
+
+#2
+
+#new_value = 255
+
+#print("0") if new_value > 100 else print("1")
+
+#3
+
+#new_value = 134
+
+#print(False) if new_value > 100 else print(True)
+
+#4
+
+#my_str = "asdf"
+
+#print(my_str + my_str) if len(my_str) < 5 else print(my_str)
+
+#5
+
+#my_str = "asdf"
+
+#print(my_str + my_str[::-1]) if len(my_str) < 5 else print(my_str)
